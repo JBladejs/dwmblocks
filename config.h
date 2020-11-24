b4ebc282a70c9bb99882f80c35c0725240fac360
Alan Ćwiek <JBladejs@gmail.com>
2020-11-24 17:24:25 +0100

    Added current date and battery status icon change

--- config.h
+++ config.h
@@ -1,10 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-        {"b: ", "battery_status", 5, 0},
+        {"", "date +'%a %d-%b-%Y %R'", 5, 0},
+        {"", "battery_status", 5, 0},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char *delim = " ";
+static char *delim = "  ";
 
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
